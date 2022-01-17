852563e53faa83866d7664420b605c65234a8756
Jad Chaar <jad.chaar@gmail.com>
2022-01-17 00:08:10 -0500

    Change JSONType to be Dict mapping string to any.

--- sec_edgar_api/_types.py
+++ sec_edgar_api/_types.py
@@ -1,5 +1,5 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List
 
-JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
+JSONType = Dict[str, Any]
 
 SubmissionsType = Dict[str, List[str]]
